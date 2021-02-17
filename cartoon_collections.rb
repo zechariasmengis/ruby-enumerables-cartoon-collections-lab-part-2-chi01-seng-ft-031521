3ac0fc172584786c32e1ea6677860ed7851d7453
Zecharias Mengis <zmengis@gmail.com>
2021-02-17 06:12:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,8 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+  planeteer_calls.map do |element|
+    element.capitalize
 end
 
 def long_planeteer_calls(planeteer_calls)
